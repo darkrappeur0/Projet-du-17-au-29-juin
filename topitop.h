26c9bf1d527d2b7033d8f2e8d238be4041bb7354
Alecstoi <paul.tournaire@sfr.fr>
2024-06-20 16:26:41 +0200

    Fichier header + premier jet PAS FONCTIONNEL de la logique de jeu

---
+++ topitop.h
@@ -0,0 +1,27 @@
+#ifndef TOPITOP_H
+#define TOPITOP_H
+
+typedef truct seau_case {
+    int pose;
+    char * couleur;
+}Seau_case;
+
+typedef struct case {
+    int x;
+    int y;
+    seau_case * seau;
+    int tour;
+    int base;
+}Case;
+
+typedef struct joueur {
+    char * couleur;
+    int nb_chateaux;
+}Joueur;
+
+Case ** genere_grille();
+Joueur * genere_joueur(char * couleur);
+int coup_valide(Joueur * j, Case * case_depart, Case * case_arrivee);
+void coup_joueur(Joueur * j, Case * case_depart, Case * case_arrivee, Case ** grid);
+int gagnant(Joueur* j);
+#endif
