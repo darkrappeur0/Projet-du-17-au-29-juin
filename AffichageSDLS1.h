c949d22bc9b1f06ab0c6d68c21ceff347e17f4dd
un_nom <un_mail>
2024-06-21 13:54:45 +0200

    Push d'une nouvelle disposition de code

--- AffichageSDLS1.h
+++ AffichageSDLS1.h
@@ -19,3 +19,23 @@ typedef struct  rectsdlneutre {
     SDL_Rect * destination4;
     int j;
 }rectsdlneutre;
+
+void deplacement(rectsdlcouleur * bleu, rectsdlcouleur * rouge,rectsdlneutre * grand, rectsdlneutre * moyen, int x, int y, float j, float i );
+void deplacementrouge(rectsdlcouleur * rouge,rectsdlneutre * grand, rectsdlneutre * moyen, int x, int y, float j, float i, int r);
+void deplacementgrand(rectsdlneutre * grand, rectsdlneutre * moyen, int x, int y, float j, float i, int r);
+void deplacementmoyen(rectsdlneutre * moyen, int x, int y, float j, float i, int r);
+int queltypecest(rectsdlcouleur * bleu, rectsdlcouleur * rouge,rectsdlneutre * grand, rectsdlneutre * moyen, int x, int y);
+int quelneutrecest(rectsdlneutre * grand, rectsdlneutre * moyen, int x, int y);
+int quelcouleurcest(rectsdlcouleur * bleu, rectsdlcouleur * rouge,int x, int y);
+int quelgrandcest(rectsdlneutre * moyen, int x, int y);
+int quelmoyencest(rectsdlneutre * moyen, int x, int y);
+int quelrougecest(rectsdlcouleur * bleu,int x, int y);
+int quelbleucest(rectsdlcouleur * bleu,int x, int y);
+rectsdlneutre * initialisationrectneutremoyen();
+rectsdlneutre * initialisationneutregrand( );
+SDL_Rect * inirectneutre2();
+SDL_Rect * inirectneutre1();
+rectsdlcouleur * initialisationrectcoulbleu( );
+rectsdlcouleur * initialisationrectcoulrouge();
+int deplacementeny(float j,int y, float i);
+int deplacementenx(float i,int x);
