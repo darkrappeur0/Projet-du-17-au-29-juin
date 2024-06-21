6096c8718c45002f0383337e4b61e47127e278f8
Alecstoi <paul.tournaire@sfr.fr>
2024-06-21 11:58:10 +0200

    Version non fonctionnelle (les inputs marchent pas)

--- PaulTournaireProjetSpriteAnim.c
+++ PaulTournaireProjetSpriteAnim.c
@@ -10,7 +10,7 @@ void BackGround (SDL_Texture *Texture, SDL_Window *Window, SDL_Renderer *Rendere
     SDL_RenderCopy(Renderer, Texture, &Source, &destination);
 }
 
-void animation(SDL_Texture *Background, SDL_Texture *Texture, SDL_Window *Window, SDL_Renderer *Renderer, int xspeed, int yspeed){
+void animation(SDL_Texture *Background, SDL_Texture *Texture, SDL_Window *Window, SDL_Renderer *Renderer, int current_x, int current_y, int xspeed, int yspeed){
     SDL_Rect Source ={0}, Window_dimensions = {0}, destination = {0};
     SDL_GetWindowSize(Window,&Window_dimensions.w, &Window_dimensions.h);
     SDL_QueryTexture(Texture, NULL, NULL,&Source.w, &Source.h);
@@ -32,56 +32,41 @@ void animation(SDL_Texture *Background, SDL_Texture *Texture, SDL_Window *Window
     }
     destination.w = offset_x * zoom;
     destination.h = offset_y * zoom;
-    destination.x = 0;
-    destination.y = 250;
+    destination.x = current_x;
+    destination.y = current_y;
     i = 0;
     if (xspeed < 0 || yspeed >0){
-        SDL_Rect destinationMirroir ={0};
-        destinationMirroir.x = destination.x+destination.w;
-        destinationMirroir.y = destination.y;
-        destinationMirroir.w = -destination.w;
-        destinationMirroir.h = destination.h;
 
         x = x + xspeed;
         y = y + yspeed;
-        destinationMirroir.x = x;
-        destinationMirroir.y = y;
+        destination.x = x;
+        destination.y = y;
         BackGround(Background,Window, Renderer); 
-        SDL_RenderCopy(Renderer,Texture, &state[i], &destinationMirroir);
+        SDL_RenderCopyEx(Renderer,Texture, &state[i], &destination, 0, NULL, 1);
         i += 1; 
         SDL_RenderPresent(Renderer);
         SDL_Delay(300);
         
         x = x + xspeed;
         y = y + yspeed;
-        destinationMirroir.x = x;
-        destinationMirroir.y = y;
+        destination.x = x;
+        destination.y = y;
         BackGround(Background,Window, Renderer); 
-        SDL_RenderCopy(Renderer,Texture, &state[i], &destinationMirroir);
+        SDL_RenderCopyEx(Renderer,Texture, &state[i], &destination, 0, NULL, 1);
         i += 1; 
         SDL_RenderPresent(Renderer);
         SDL_Delay(300);
 
         x = x + xspeed;
         y = y + yspeed;
-        destinationMirroir.x = x;
-        destinationMirroir.y = y;
+        destination.x = x;
+        destination.y = y;
         BackGround(Background,Window, Renderer); 
-        SDL_RenderCopy(Renderer,Texture, &state[i], &destinationMirroir);
-        i += 1; 
+        SDL_RenderCopyEx(Renderer,Texture, &state[i], &destination, 0, NULL, 1);
         SDL_RenderPresent(Renderer);
         SDL_Delay(300);
     }
     else {
-        x = x + xspeed;
-        y = y + yspeed;
-        destination.x = x;
-        destination.y = y;
-        BackGround(Background,Window, Renderer); 
-        SDL_RenderCopy(Renderer,Texture, &state[i], &destination);
-        i += 1; 
-        SDL_RenderPresent(Renderer);
-        SDL_Delay(300);
 
         x = x + xspeed;
         y = y + yspeed;
@@ -100,6 +85,15 @@ void animation(SDL_Texture *Background, SDL_Texture *Texture, SDL_Window *Window
         BackGround(Background,Window, Renderer); 
         SDL_RenderCopy(Renderer,Texture, &state[i], &destination);
         i += 1; 
+        SDL_RenderPresent(Renderer);
+        SDL_Delay(300);
+
+        x = x + xspeed;
+        y = y + yspeed;
+        destination.x = x;
+        destination.y = y;
+        BackGround(Background,Window, Renderer); 
+        SDL_RenderCopy(Renderer,Texture, &state[i], &destination);
         SDL_RenderPresent(Renderer);
         SDL_Delay(300);
     }
@@ -116,15 +110,55 @@ int main(){
     sp_st = IMG_LoadTexture(Renderer, "Slime_Still(1).png");
     sp_mo = IMG_LoadTexture(Renderer, "Slime_Advancing(1).png");
     bg = IMG_LoadTexture(Renderer, "Flowery_Floor.png");
+    SDL_Rect Pos = {0}, window_dimension = {0};
+    int x = 300;
+    int y = 300;
+    SDL_GetWindowSize(Win, &window_dimension.w, &window_dimension.h);
+    const Uint8* keystates;
     SDL_bool running = SDL_TRUE;
     while (running){
-        SDL_Event event;
+        keystates = SDL_GetKeyboardState(NULL);
+        if (keystates[SDL_SCANCODE_ESCAPE]){
+            running = SDL_FALSE;
+        }
+        else if (keystates[SDL_SCANCODE_UP]){
+            animation(bg, sp_mo, Win, Renderer, x, y, 0, -10);
+            SDL_QueryTexture(sp_mo, NULL, NULL, &Pos.w, &Pos.h);
+            x = window_dimension.w - Pos.w;
+            y = window_dimension.h - Pos.h;
+        }
+        else if (keystates[SDL_SCANCODE_RIGHT]){
+            animation(bg, sp_mo, Win, Renderer, x, y, 10, 0);
+            SDL_QueryTexture(sp_mo, NULL, NULL, &Pos.w, &Pos.h);
+            x = window_dimension.w - Pos.w;
+            y = window_dimension.h - Pos.h;
+        }
+        else if (keystates[SDL_SCANCODE_DOWN]){
+            animation(bg, sp_mo, Win, Renderer, x, y, 0, 10);
+            SDL_QueryTexture(sp_mo, NULL, NULL, &Pos.w, &Pos.h);
+            x = window_dimension.w - Pos.w;
+            y = window_dimension.h - Pos.h;
+        }
+        else if (keystates[SDL_SCANCODE_LEFT]){
+            animation(bg, sp_mo, Win, Renderer, x, y, -10, 0);
+            SDL_QueryTexture(sp_mo, NULL, NULL, &Pos.w, &Pos.h);
+            x = window_dimension.w - Pos.w;
+            y = window_dimension.h - Pos.h;
+        }
+        else {
+            animation(bg, sp_st, Win, Renderer, x, y, 0, 0);
+            SDL_QueryTexture(sp_st, NULL, NULL, &Pos.w, &Pos.h);
+            x = window_dimension.w - Pos.w;
+            y = window_dimension.h - Pos.h;
+        }
+    }
+/*        SDL_Event event;
         while (SDL_PollEvent(&event)){
             switch (event.type){
                 case SDL_QUIT :
                     running = SDL_FALSE;
                     break;
-/*                case SDL_KEYDOWN :
+                case SDL_KEYDOWN :
                     switch (event.key.keysym.sym){
                         case SDLK_UP :
                             animation(bg, sp_mo, Win, Renderer, 0, -10);
@@ -142,7 +176,7 @@ int main(){
                             break;
                     }
                     break;
-*/                default :
+               default :
                     animation(bg, sp_mo, Win, Renderer, 0, -10);
                     SDL_Delay(100);
                     animation(bg, sp_mo, Win, Renderer, 10, 0);
@@ -156,7 +190,7 @@ int main(){
                     break;
             }
         }
-    }
+    }*/
     
     SDL_DestroyRenderer(Renderer);
     SDL_DestroyWindow(Win);
