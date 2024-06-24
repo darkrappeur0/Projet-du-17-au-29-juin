e0421a4f0120a08e043d67680666934d11efca17
Alecstoi <paul.tournaire@sfr.fr>
2024-06-24 21:11:12 +0200

    Affichage fin de journée de travail 24062024

--- InterfaceGraphique.c
+++ InterfaceGraphique.c
@@ -2,8 +2,18 @@
 #include <stdlib.h>
 #include "InterfaceGraphique.h"
 
-SDL_Rect *  Load_Cards(SDL_Texture * paquet, SDL_Renderer * renderer){
+void SetMat(SDL_Texture * bg, SDL_Renderer * renderer, SDL_Window * window){
+    SDL_Rect Source = {0}, window_dimensions = {0}, destination = {0};
+    SDL_GetWindowSize(window, &window_dimensions.w, &window_dimensions.h);
+    SDL_QueryTexture(bg, NULL, NULL, &source.w, &source.h);
+    destination = window_dimensions;
+    SDL_RenderCopy(renderer, texture, &source, &destination);
+}
+
+SDL_Rect *  Load_Cards(SDL_Texture * paquet, SDL_Renderer * renderer, SDL_Window * window){
     paquet = IMG_LoadTexture(renderer,'./ImagesWizard/FullJeuDuWizard.png');
-    SDL_Rect source = {0}, dimensions_window = {0}, destination = {0};
+    SDL_Rect source = {0}, window_dimensions = {0}, destination = {0};
+    float zoom = 8;
+    int deck = 54;
     
 }
