f63ac37c1de3b1e5265d8e10f870bcb958cc2ae8
un_nom <un_mail>
2024-06-24 16:44:56 +0200

    Push de la première partie logique

---
+++ plis.h
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+typedef struct carte {
+    int couleur;
+    int num;
+}carte;
+
+typedef struct deck{
+    int nb_de_carte;
+    carte * carte;
+    struct deck * next; 
+}deck;
+
+typedef struct joueur{
+    int joueur;
+    int nb_de_carte;
+    int nb_de_plis_predit;
+    int nb_de_plis_fait;
+}joueur;
+
+
+typedef struct prediction{
+    int nb_plit_j1;
+    int nb_plit_j2;
+    int nb_plit_preditj1;
+    int nb_plit_preditj2;
+}prediction;
+
+
+
+
+
+
+carte * generecarte();
+carte * generecartesansdoublons(deck * deck,int r);
+void displaycarte(carte * carte);
+void displaydeck(deck * deck);
+deck * generedeck(int nbdecarte);
+int moyenne1plis( int tab[][14],int n);
+int evalplis( carte * cartejouerj1, carte * cartejouerj2, int atout,int premierecarte);
+int prediction1plis(carte * cartejouerj1,int atout,int premierecarte);
+int predictionplistotal(deck * main1,int atout,int premierecarte);
+void plispredic(deck * main, int atout, int premierecarte, prediction * plispredi, int j );
---
+++ score.h
@@ -0,0 +1,16 @@
+#include "plis.h"
+
+typedef struct score {
+    int scorej1;
+    int scorej2;
+    int nb_de_carte;
+}score;
+
+
+
+
+
+
+score * creescore();
+int reglescore(int nb_plit_reel,int nb_plit_predit);
+score * update_score (int nb_plit_j1, int nb_plit_j2, int nb_plit_preditj1, int nb_plit_preditj2, score * sprev );
