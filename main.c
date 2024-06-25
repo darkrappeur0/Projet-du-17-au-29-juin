50d730a7a54d824f3af83adf8a357f15f2658a9a
Alecstoi <paul.tournaire@sfr.fr>
2024-06-25 17:05:19 +0200

    main qui marche pour l'affichage (problème de déclaration des prototypes de score.c)

--- main.c
+++ main.c
@@ -40,7 +40,7 @@ int main(){
     SDL_Init(SDL_INIT_EVERYTHING);
     TTF_Init();
     SDL_Window * window = SDL_CreateWindow("Jouez au Wizard", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 64, 36, SDL_WINDOW_MAXIMIZED);
-    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, NULL);
+    SDL_Renderer * renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_PRESENTVSYNC);
     SDL_Texture * bakgrond = IMG_LoadTexture(renderer, "./ImagesWizard/Tapis.png");
     SetMat(bakgrond, renderer, window);
     SDL_RenderPresent(renderer);
