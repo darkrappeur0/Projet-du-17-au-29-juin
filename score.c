0d5260358380ab87602e15f0b5b0ffbf8f11de35
un_nom <un_mail>
2024-06-25 14:02:28 +0200

    update main

--- score.c
+++ score.c
@@ -43,7 +43,7 @@ score * update_score2 (score * snouv, score * sprev ){
     return s;
 }
 
-//
+
 
 score * unepartie(deck * deckIA, int atout, int premierecarte){
     prediction * p = malloc(sizeof(prediction));
@@ -71,30 +71,3 @@ void displayscore(score * s){
 
 // ameliorer l'allocation du tableau ou on stock les données des plis.
 
-
-int main(){
-    score * s =  creescore();
-    int i =1;
-    score * spartie =  creescore();
-    while (i != 10){
-        srand(time(NULL));
-        deck * coucou = generedeck(i,NULL);
-        spartie->nb_de_carte = i;
-        int d = i% 2;
-        d=d+1;
-        spartie = unepartie(coucou, 1, d);
-        if (i == 1){
-            s = spartie;
-        }
-        else{
-            s = update_score2(spartie,s);
-            spartie = s;
-        }
-        displayscore(s);
-        i=i+1;
-        freedeck(coucou);
-    }
-    free(s);
-    
-    return 0;
-}
